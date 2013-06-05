afcb6ca28306233e5bfbe8194cc9564ea4a2f570
alex z <t2kalex@icloud.com>
2013-06-05 18:00:09 +0300

    milestone1

---
+++ client/view controller/aszChapterOverviewViewController.h
@@ -0,0 +1,13 @@
+//
+//  aszChapterOverviewViewController.h
+//  DTPLight
+//
+//  Created by alex zaikman on 6/5/13.
+//  Copyright (c) 2013 alex zaikman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface aszChapterOverviewViewController : UIViewController
+
+@end
---
+++ client/view controller/aszChapterOverviewViewController.m
@@ -0,0 +1,38 @@
+//
+//  aszChapterOverviewViewController.m
+//  DTPLight
+//
+//  Created by alex zaikman on 6/5/13.
+//  Copyright (c) 2013 alex zaikman. All rights reserved.
+//
+
+#import "aszChapterOverviewViewController.h"
+
+@interface aszChapterOverviewViewController ()
+
+@end
+
+@implementation aszChapterOverviewViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
